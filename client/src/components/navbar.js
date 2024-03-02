c1c1cf7469de6155ac329effb0ad1c8f050f7df6
tylerteichmann <tyler.j.teichmann@gmail.com>
2024-03-02 14:02:08 -0800

    final touches

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -14,7 +14,6 @@ export default function Navbar() {
                 <NavLink className="navbar-brand" to="/">
                     <img style={{"width" : 25 + '%'}} src="https://d3cy9zhslanhfa.cloudfront.net/media/3800C044-6298-4575-A05D5C6B7623EE37/4B45D0EC-3482-4759-82DA37D8EA07D229/webimage-8A27671A-8A53-45DC-89D7BF8537F15A0D.png" alt="leaf"></img>
                 </NavLink>
-
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -23,18 +22,17 @@ export default function Navbar() {
                     aria-controls="navbarSupportedContent"
                     aria-expanded="false"
                     aria-label="Toggle navigation"
-                >
+                    >
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/create">
-                                Create Record
-                            </NavLink>
+                            <NavLink className="nav-link" to="/create">Create Record </NavLink>
                         </li>
                     </ul>
+
                 </div>
             </nav>
         </div>
